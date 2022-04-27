811b84af6044ffde31f3bb5be7b2b19b889f8e23
Tim Yang <tim.yang@tech-trans.com>
2022-04-27 08:39:07 +0800

    feat: 新增md-editor功能

--- projects/demo1/src/app/app.component.ts
+++ projects/demo1/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdEditorOption, UploadResult } from 'ngx-markdown-editor/dist';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,122 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo1';
+  public options: MdEditorOption = {
+    showPreviewPanel: false,
+    enablePreviewContentClick: false,
+    resizable: true,
+    customRender: {
+      image: function (href: string, title: string, text: string) {
+        let out = `<img style="max-width: 100%; border: 20px solid red;" src="${href}" alt="${text}"`;
+        if (title) {
+          out += ` title="${title}"`;
+        }
+        out += (<any>this.options).xhtml ? "/>" : ">";
+        return out;
+      }
+    },
+    markedjsOpt: {
+      sanitize: true
+    }
+  };
+  public content: string;
+  public mode: string = 'editor';
+
+  constructor(private _domSanitizer: DomSanitizer) {
+    this.preRender = this.preRender.bind(this);
+    this.postRender = this.postRender.bind(this);
+  }
+
+  ngOnInit() {
+    let contentArr = ['# Hello, Markdown Editor!'];
+    contentArr.push('```javascript ');
+    contentArr.push('function Test() {');
+    contentArr.push('	console.log("Test");');
+    contentArr.push('}');
+    contentArr.push('```');
+    contentArr.push(' Name | Type');
+    contentArr.push(' ---- | ----');
+    contentArr.push(' A | Test');
+    contentArr.push('![](http://lon-yang.github.io/markdown-editor/favicon.ico)');
+    contentArr.push('');
+    contentArr.push('- [ ] Taks A');
+    contentArr.push('- [x] Taks B');
+    contentArr.push('- test');
+    contentArr.push('');
+    contentArr.push('[Link](https://www.google.com)');
+    contentArr.push(`<img src="1" onerror="alert(1)" />`);
+    contentArr.push('');
+    this.content = contentArr.join('\r\n');
+  }
+
+  togglePreviewPanel() {
+    this.options.showPreviewPanel = !this.options.showPreviewPanel;
+    this.options = Object.assign({}, this.options);
+  }
+
+  changeMode() {
+    if (this.mode === 'editor') {
+      this.mode = 'preview';
+    } else {
+      this.mode = 'editor';
+    }
+  }
+
+  togglePreviewClick() {
+    this.options.enablePreviewContentClick = !this.options.enablePreviewContentClick;
+    this.options = Object.assign({}, this.options);
+  }
+
+  toggleResizeAble() {
+    this.options.resizable = !this.options.resizable;
+    this.options = Object.assign({}, this.options);
+  }
+
+  doUpload(files: Array<File>): Promise<Array<UploadResult>> {
+    console.log(files);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let result: Array<UploadResult> = [];
+        for (let file of files) {
+          result.push({
+            name: file.name,
+            url: `https://avatars3.githubusercontent.com/${file.name}`,
+            isImg: file.type.indexOf('image') !== -1
+          })
+        }
+        resolve(result);
+      }, 3000);
+    });
+  }
+
+  onEditorLoaded(editor) {
+    console.log(`ACE Editor Ins: `, editor);
+    // editor.setOption('showLineNumbers', false);
+
+    // setTimeout(() => {
+    //   editor.setOption('showLineNumbers', true);
+    // }, 2000);
+  }
+
+  preRender(mdContent) {
+    console.log(`preRender fired`);
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve(mdContent);
+    //   }, 4000);
+    // })
+    return mdContent;
+  }
+
+  postRender(html) {
+    console.log(`postRender fired`);
+    // return '<h1>Test</h1>';
+    return html;
+  }
+
+  onPreviewDomChanged(dom: HTMLElement) {
+    console.log(`onPreviewDomChanged fired`);
+    // console.log(dom);
+    // console.log(dom.innerHTML)
+  }
 }
