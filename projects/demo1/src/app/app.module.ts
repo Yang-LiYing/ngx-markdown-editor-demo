115d1069543976c0aa929b6768623c4bd4f4e8d8
Tim Yang <tim.yang@tech-trans.com>
2022-04-27 21:41:46 +0800

    fix: ace.js改為APP_INITIALIZER時加載

--- projects/demo1/src/app/app.module.ts
+++ projects/demo1/src/app/app.module.ts
@@ -1,9 +1,35 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule, Provider } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { LMarkdownEditorModule } from 'ngx-markdown-editor/dist';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+function loadExternalScript(url: string): Promise<void> {
+  return new Promise<void>((res,rej) => {
+    const node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    node.async = false;
+    document.getElementsByTagName('head')[0].appendChild(node);
+    node.onload = () => {
+      res();
+    }
+    node.onerror = () => {
+      rej();
+    }
+  });
+}
+
+const ACE_JS_LOADER: Provider = {
+  provide: APP_INITIALIZER,
+  multi: true,
+  useFactory: () => {
+    return () => {
+      return loadExternalScript('/assets/ace-builds/ace.js');
+    }
+  },
+}
 
 @NgModule({
   declarations: [
@@ -15,7 +41,9 @@ import { AppComponent } from './app.component';
     FormsModule, // make sure FormsModule is imported to make ngModel work
     LMarkdownEditorModule
   ],
-  providers: [],
+  providers: [
+    ACE_JS_LOADER
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
